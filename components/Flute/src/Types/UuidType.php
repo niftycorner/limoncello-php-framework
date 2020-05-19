e7395f87b6e9863563c6a9eeaece0f123dd2f005
dreamsbond <info@dreamsbond.com>
2020-05-19 11:11:39 +0800

    update.

--- components/Flute/src/Types/UuidType.php
+++ components/Flute/src/Types/UuidType.php
@@ -2,56 +2,13 @@
 
 namespace Limoncello\Flute\Types;
 
-use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Types\GuidType;
-use Ramsey\Uuid\Uuid;
-use Ramsey\Uuid\UuidInterface;
-
 /**
  * @package App
  */
-class UuidType extends GuidType
+class UuidType extends \Ramsey\Uuid\Doctrine\UuidType
 {
     /**
      * Type name
      */
     const NAME = 'limoncelloUuid';
-
-    /**
-     * @inheritDoc
-     */
-    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
-    {
-        $result = null;
-
-        if (empty($value))
-            $result = null;
-
-        if (($value instanceof UuidInterface) || ((is_string($value) || method_exists($value, '__toString')) && Uuid::isValid((string)$value)))
-            $result = (string)$value;
-
-        return $result;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function convertToPHPValue($value, AbstractPlatform $platform): ?UuidInterface
-    {
-        $result = null;
-
-        if (empty($value)) {
-            $result = null;
-        }
-
-        if ($value instanceof UuidInterface) {
-            $result = $value;
-        }
-
-        if (is_string($value)) {
-            $result = Uuid::fromString($value);
-        }
-
-        return $result;
-    }
 }
